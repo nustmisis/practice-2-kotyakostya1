6916e1b4e5637e33898b72065fd6eb46765741c8
kotyakostya1 <56290301+kotyakostya1@users.noreply.github.com>
2023-10-07 17:07:26 +0300

    task 4.py

--- task 4.py
+++ task 4.py
@@ -5,25 +5,14 @@
 для поиска в списке тепеней 2 занания 2, вовзевдённую в пятую степень
 
 @author: workk
-"""
-
-L = [1, 2, 4, 8, 16, 32, 64]
+"""    
+L =[2**i for i in range(7)]
 X = 5
-found = False
-i = 0
-while not found and i < len(L) :
-    if 2 ** X == L[i] :
-        found = True
-    else:
-        i = i + 1
 
-if found:
-    print('at index', i)
+if 2 ** X in L:
+        print('at index', L.index(2 ** X))
 else:
-    print(X, 'not found')
-    
-    
-    
+    print(X, 'not found')    
 '''
 Код явно написан с использование альетрнативной логики.
 Попоробуйте оптимизировать код c использование рекомендаций, ониявляются не обезатальными, но помогут понять основные ошибки.
